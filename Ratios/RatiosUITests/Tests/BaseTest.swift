a9f5b62602cc72ceb361c03a1ec73dba1e190a19
leonardust <luk.komorowski@gmail.com>
2023-05-08 11:28:15 +0200

    last fix...

--- Ratios/RatiosUITests/Tests/BaseTest.swift
+++ Ratios/RatiosUITests/Tests/BaseTest.swift
@@ -20,5 +20,23 @@ class BaseTest: XCTestCase {
     override func tearDown() {
         app = nil
     }
+
+    enum ElementType {
+        case staticText
+        case button
+    }
+
+    func waitForElement(forElementWithID elementID: String, type: ElementType, timeout: TimeInterval) -> Bool {
+        switch type {
+        case .staticText:
+            let element = XCUIApplication().staticTexts[elementID]
+            return element.waitForExistence(timeout: timeout)
+        case .button:
+            let element = XCUIApplication().buttons[elementID]
+            return element.waitForExistence(timeout: timeout)
+        default:
+            fatalError("Unknown element type: \(type)")
+        }
+    }
     
 }
